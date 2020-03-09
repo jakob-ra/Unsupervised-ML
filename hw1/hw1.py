9cb28d896103c6244ddd67140926594936b9404e
jakob-ra <jakob.rauch@student.uva.nl>
2020-03-09 11:51:40 +0100

    setup

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -1,8 +1,6 @@
 # Imports
-import pyreadr
 import pandas as pd
 
 # Read data
-url = 'https://github.com/jakob-ra/Unsupervised-ML/raw/master/hw1/FIFA2017_NL.RData'
-data = pyreadr.read_r(url) # also works for Rds
-data = pyreadr.read_r('../hw1/fifa.RData') # also works for Rds
+url = 'https://github.com/jakob-ra/Unsupervised-ML/raw/master/fifa.xlsx'
+df = pd.read_excel(url)
